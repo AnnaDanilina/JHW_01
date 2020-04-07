778e4df7c2ee50decc9fc24ed3d836136fc38bb3
AnnaDanilina <avd09@mail.ru>
2020-04-07 21:48:33 +0300

    Домашнее задание 08

--- src/HW08/Window.java
+++ src/HW08/Window.java
@@ -2,43 +2,15 @@ package HW08;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.Random;
 
 public class Window extends JFrame {
-
-    private JPanel getPanelWithBorderLayout() {
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(new JButton("OK"), BorderLayout.WEST);
-        panel.add(new JTextArea(), BorderLayout.CENTER);
-        panel.add(new JButton("OK"), BorderLayout.EAST);
-        panel.add(new JButton("OK"), BorderLayout.NORTH);
-        panel.add(new JButton("OK"), BorderLayout.SOUTH);
-        return panel;
-    }
-
-    public JPanel getPanelWithFlowLayout() {
-        JPanel panel = new JPanel(new FlowLayout());
-        panel.add(new JButton("OK"));
-        panel.add(new JTextArea("asfasgasgasg"));
-        panel.add(new JButton("OK"));
-        panel.add(new JButton("OK"));
-        panel.add(new JButton("OK"));
-        return panel;
-    }
-
-    public JPanel getPanelWithGridLayout(int row, int col) {
-        JPanel panel = new JPanel(new GridLayout(row, col));
-        panel.add(new JButton("OK"));
-        panel.add(new JTextArea("asfasgasgasg"));
-        panel.add(new JButton("OK"));
-        panel.add(new JButton("OK"));
-        panel.add(new JButton("OK"));
-        return panel;
-    }
-
+    static Random rand = new Random();
     private final ImageIcon imageX = new ImageIcon("src/HW08/x.png");
     private final ImageIcon imageO = new ImageIcon("src/HW08/o.png");
-    private final ImageIcon imageEmpty = new ImageIcon("src/HW08/empty.png");
+    private final ImageIcon imageEmpty = new ImageIcon("src/HW08/empty.png ");
     private int cnt = 0;
+    boolean gameOver = false;
 
     public Window() throws HeadlessException {
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -58,28 +30,22 @@ public class Window extends JFrame {
                     copy.setEnabled(false);
                     cnt++;
                     copy.setDisabledIcon(imageX);
-                    movePC(buttons);
-                    if (cnt == 9) {
-                        JFrame alert = new JFrame("кто то победил или ничья");
-                        alert.setLocation(500, 400);
-                        alert.setSize(300, 100);
-                        JPanel alertPanel = new JPanel(new FlowLayout());
-                        JButton newGame = new JButton("new game");
-                        newGame.addActionListener(a -> {
-                            this.dispose();
-                            new Window();
-                            alert.dispose();
-                        });
-                        JButton close = new JButton("close");
-                        close.addActionListener(a -> {
-                            dispose();
-                            alert.dispose();
-                        });
-                        alertPanel.add(newGame);
-                        alertPanel.add(close);
-                        alert.add(alertPanel);
-                        alert.setResizable(false);
-                        alert.setVisible(true);
+                    // Проверку не выиграл ли Х
+                    if (isVictory(buttons, imageX)) {
+                        finishGame("Победил игрок");
+                        gameOver = true;
+                    }
+
+                    if (cnt != 9 && !gameOver) {
+                        movePC(buttons);
+                        // Проверку не выиграл ли О
+                        if (isVictory(buttons, imageO)) {
+                            finishGame("Победил комп");
+                            gameOver = true;
+                        }
+                    }
+                    if (cnt == 9 && !gameOver) {
+                        finishGame("Ничья");
                     }
                 });
                 panel.add(buttons[i][j]);
@@ -89,18 +55,100 @@ public class Window extends JFrame {
         setVisible(true);
     }
 
+    public void finishGame(String msg){
+        JFrame alert = new JFrame(msg);
+        alert.setLocation(500, 400);
+        alert.setSize(300, 100);
+        JPanel alertPanel = new JPanel(new FlowLayout());
+        JButton newGame = new JButton("new game");
+        newGame.addActionListener(a -> {
+            this.dispose();
+            new Window();
+            alert.dispose();
+        });
+        JButton close = new JButton("close");
+        close.addActionListener(a -> {
+            dispose();
+            alert.dispose();
+        });
+        alertPanel.add(newGame);
+        alertPanel.add(close);
+        alert.add(alertPanel);
+        alert.setResizable(false);
+        alert.setVisible(true);
+    }
+
     private void movePC(JButton[][] buttons) {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                if (buttons[i][j].isEnabled()) {
-                    buttons[i][j].setIcon(imageO);
-                    buttons[i][j].setEnabled(false);
-                    buttons[i][j].setDisabledIcon(imageO);
-                    cnt++;
-                    return;
-                }
+        int x, y;
+        do {
+            x = rand.nextInt(3);
+            y = rand.nextInt(3);
+        } while (!buttons[x][y].isEnabled());
+        buttons[x][y].setIcon(imageO);
+        buttons[x][y].setEnabled(false);
+        buttons[x][y].setDisabledIcon(imageO);
+        cnt++;
+        return;
+
+    }
+
+    private boolean isVictory(JButton[][] tab,  ImageIcon image/*X, O*/) {
+        for (int i = 0; i < tab.length; i++) {
+            for (int j = 0; j < tab[i].length; j++) {
+                if (checkLine(tab, i, j, image)) return true;
             }
         }
+        return false;
+    }
+
+    public boolean checkLine( JButton[][] tab, int x, int y, ImageIcon image) {
+        boolean b;
+        int field_size = 3;
+        int dot_win = 3;
+        // прямые
+        if (tab[x][y].getIcon() != image) return false;
+        if (field_size - x >= 3) {
+            b = true;
+            for (int i = 0; i < dot_win; i++) {
+                if (tab[i + x][y].getIcon() != image) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) return true;
+        }
+        if (field_size - y >= dot_win) {
+            b = true;
+            for (int i = 0; i < dot_win; i++) {
+                if (tab[x][y + i].getIcon() != image) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) return true;
+        }
+
+        if (field_size - y >= dot_win && field_size - x >= dot_win) {
+            b = true;
+            for (int i = 0; i < dot_win; i++) {
+                if (tab[x + i][y + i].getIcon() != image) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) return true;
+        }
+        if (field_size - y >= dot_win && 1 + x >= dot_win) {
+            b = true;
+            for (int i = 0; i < dot_win; i++) {
+                if (tab[x - i][y + i].getIcon() != image) {
+                    b = false;
+                    break;
+                }
+            }
+            if (b) return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
